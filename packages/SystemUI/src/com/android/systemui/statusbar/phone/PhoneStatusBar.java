39ad21cbff521af7ff326003925a39b04de71060
Justin Driggers <justin@justindriggers.com>
2015-11-25 01:01:18 -0500

    Revert "WIP: draggable quick settings tiles"
    This reverts commit 8b59e888aa4236f29728785b1908c06989ec6f8a.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -135,7 +135,6 @@ import com.android.systemui.cm.UserContentObserver;
 import com.android.systemui.doze.DozeHost;
 import com.android.systemui.doze.DozeLog;
 import com.android.systemui.keyguard.KeyguardViewMediator;
-import com.android.systemui.qs.QSDragPanel;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.settings.BrightnessController;
@@ -342,7 +341,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     TextView mNotificationPanelDebugText;
 
     // settings
-    private QSDragPanel mQSPanel;
+    private QSPanel mQSPanel;
     private DevForceNavbarObserver mDevForceNavbarObserver;
 
     // top bar
@@ -1123,7 +1122,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
 
         // Set up the quick settings tile panel
-        mQSPanel = (QSDragPanel) mStatusBarWindowContent.findViewById(R.id.quick_settings_panel);
+        mQSPanel = (QSPanel) mStatusBarWindowContent.findViewById(R.id.quick_settings_panel);
         if (mQSPanel != null) {
             final QSTileHost qsh = new QSTileHost(mContext, this,
                     mBluetoothController, mLocationController, mRotationLockController,
@@ -1139,27 +1138,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             qsh.setCallback(new QSTileHost.Callback() {
                 @Override
                 public void onTilesChanged() {
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mQSPanel.setTiles(qsh.getTiles());
-                        }
-                    });
-                }
-
-                @Override
-                public void setEditing(final boolean editing) {
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mQSPanel.setEditing(editing);
-                        }
-                    });
-                }
-
-                @Override
-                public boolean isEditing() {
-                    return mQSPanel.isEditing();
+                    mQSPanel.setTiles(qsh.getTiles());
                 }
             });
         }
